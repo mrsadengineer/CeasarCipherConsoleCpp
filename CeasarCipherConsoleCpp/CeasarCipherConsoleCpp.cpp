7852b18d093c057663fdd02f078bce5963bca940
Sammy <sammy.davidson@hotmail.com>
2020-10-25 20:59:28 -0400

    added while loop at the beginning to create interactive prompt display commands

--- CeasarCipherConsoleCpp/CeasarCipherConsoleCpp.cpp
+++ CeasarCipherConsoleCpp/CeasarCipherConsoleCpp.cpp
@@ -3,12 +3,53 @@
 
 #include <iostream>
 
+int processCommand();
+
+
+
 int main()
 {
-    std::cout << "Ceasar Cipher Console App!\n";
-    std::cout << "Press A to Cipher\n";
-    std::cout << "Press B to Decipher\n";
+
+    int whatCommand;
+
+    whatCommand = processCommand();
+
+    while (whatCommand != 3)
+    {
+        switch (whatCommand)
+        {
+        case 1:
+            std::cout << "lets cipher something\n";
+            break;
+        case 2:
+            std::cout << "lets decipher something\n";
+            break;
+        case 3:
+            std::cout << "should exit\n";
+            break;
+        default:
+            break;
+        }
+
+        whatCommand = processCommand();
+    }
+
+
+    return 0;
+
         
+}
+
+
+int processCommand() {
+    int choice;
+   std::cout << "Ceasar Cipher Console App!\n";
+    std::cout << "Press 1 to Cipher\n";
+    std::cout << "Press 2 to Decipher\n";
+    std::cout << "Press 3 to exit\n";
+    std::cin >> choice;
+    return choice;
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
